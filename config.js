b47b089f8931a2a3d3916e0cb8e72a68049302fb
Alex Solomaha <cyanofresh@gmail.com>
2018-03-11 18:41:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.telegram = {};
-config.telegram.bot_token = '491810050:AAG3hTlm-tBEzVYxtGn_7NmsSTWlkO9JyKE';
+config.telegram.bot_token = '';
 config.telegram.chat_id = 302366401;
 
 config.ws = {};
